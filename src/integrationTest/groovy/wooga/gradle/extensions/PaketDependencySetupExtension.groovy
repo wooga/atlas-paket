728e96741307e63fc25abaeabc668821ec6b7e89
Manfred Endres <2523575+Larusso@users.noreply.github.com>
2019-01-02 13:07:44 +0000

    Refactor integration spec for paket unity change specs (#47)
    Description
    ===========
    
    This patch moves some boilerplate setup code into custom spock
    extensions. This patch adds two extensions that provide a clean
    interface for testing paket and paket unity project integrations:
    
    * @PaketDependency
    * @PaketUnity
    
    @PaketDependency
    ----------------
    
    Is a resource extension which setups a paket project.
    It will create a `paket.dependendies` along with a `paket.lock` file and
    stub `cs` files in `packages` directory. This extensions should be
    updated for future use in other tests (don't create a lock file,
    specify sources, etc).
    
    ```java
    @ExtensionAnnotation(PaketDependencySetupExtension)
    @interface PaketDependency {
        String[] projectDependencies() default []
    }
    ```
    
    ```groovy
    class ASpec extends IntegrationSpec {
        @PaketDependency(projectDependencies = ["D1", "D2", "D3"])
        PaketDependencySetup paketSetup
    ```
    
    The annotated field will turn into a an Object of `PaketDependencySetup`
    It is possible to override and regenerate the dependencies during test runtime.
    
    ```groovy
    interface PaketDependencySetup {
        File getPaketDependencies()
        File getPaketLock()
    
        List<String> getProjectDependencies()
    
        File createDependencies()
        File createDependencies(List<String> dependencies)
    }
    ```
    
    @PaketUnity
    -----------
    
    The paket unity extension creates a mock setup for unity. Each field
    annotated with `@PaketUnity` will generate a mock unity project.
    
    ```java
    @ExtensionAnnotation(PaketUnitySetupExtension)
    @interface PaketUnity {
        String projectName() default ""
        String[] projectReferences() default []
    }
    ```
    
    ```groovy
    class ASpec extends IntegrationSpec {
        @PaketUnity(projectReferences = ["D1", "D2", "D3"])
        PaketUnitySetup unityProject1
    ```
    
    The annotated field will turn into a an Object of `PaketUnitySetup`
    It is possible to override and regenerate the references during test runtime.
    
    ```groovy
    interface PaketUnitySetup {
        File getProjectReferencesFile()
        File getInstallDirectory()
        File getUnityProjectDir()
        String getName()
    
        List<String> getProjectReferences()
    
        File createOrUpdateReferenceFile()
        File createOrUpdateReferenceFile(List<String> projectReferences)
    }
    ```
    
    Changes
    =======
    
    ![ADD] `@PaketDependency` spock test extension
    ![ADD] `@PaketUnity` spock test extension
    ![IMPROVE] integration specs

---
+++ src/integrationTest/groovy/wooga/gradle/extensions/PaketDependencySetupExtension.groovy
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2018 Wooga GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package wooga.gradle.extensions
+
+import groovy.transform.InheritConstructors
+import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension
+import org.spockframework.runtime.extension.IMethodInvocation
+import org.spockframework.runtime.model.FieldInfo
+
+class PaketDependencySetupExtension extends AbstractAnnotationDrivenExtension<PaketDependency> {
+
+    @Override
+    void visitFieldAnnotation(PaketDependency annotation, FieldInfo field) {
+        def interceptor
+        interceptor = new PaketDependencyInterceptor(field.name, annotation.projectDependencies())
+        interceptor.install(field.parent.getTopSpec())
+        interceptor.install(field.parent.getBottomSpec())
+    }
+}
+
+@InheritConstructors
+class PaketDependencyInterceptor extends GradleIntegrationSpecInterceptor implements PaketDependencySetup {
+
+    List<String> projectDependencies
+
+    protected File paketDependencies
+    protected File paketLock
+
+    PaketDependencyInterceptor(String fieldName, String[] projectDependencies) {
+        super(fieldName)
+        this.projectDependencies = projectDependencies.toList()
+    }
+
+    @Override
+    File getPaketDependencies() {
+        paketDependencies
+    }
+
+    @Override
+    File getPaketLock() {
+        paketLock
+    }
+
+    void setupProject(IMethodInvocation invocation) {
+        createDependencies()
+    }
+
+    File createDependencies() {
+        def dependenciesFile = createFile("paket.dependencies")
+        dependenciesFile.text = ""
+        dependenciesFile << """source https://nuget.org/api/v2
+nuget ${projectDependencies.join("\nnuget ")}""".stripIndent()
+
+        projectDependencies.each { dependency ->
+            createFile("packages/${dependency}/content/ContentFile.cs")
+        }
+        createLockFile(projectDependencies)
+        paketDependencies = dependenciesFile
+        dependenciesFile
+    }
+
+    File createDependencies(List<String> dependencies) {
+        projectDependencies = dependencies
+        createDependencies()
+    }
+
+    void createLockFile(List<String> dependencies) {
+        def lockFile = createFile("paket.lock")
+        lockFile.text = ""
+        lockFile << """
+NUGET
+    remote: https://wooga.artifactoryonline.com/wooga/api/nuget/atlas-nuget
+        ${dependencies.join("\n")}""".stripIndent()
+        paketLock = lockFile
+    }
+}
---
+++ src/integrationTest/groovy/wooga/gradle/extensions/PaketUnitySetupExtension.groovy
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 Wooga GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package wooga.gradle.extensions
+
+import groovy.transform.InheritConstructors
+import nebula.test.IntegrationSpec
+import org.spockframework.runtime.extension.AbstractAnnotationDrivenExtension
+import org.spockframework.runtime.extension.AbstractMethodInterceptor
+import org.spockframework.runtime.extension.IMethodInvocation
+import org.spockframework.runtime.model.FieldInfo
+import org.spockframework.runtime.model.SpecInfo
+import wooga.gradle.paket.base.internal.DefaultPaketPluginExtension
+import wooga.gradle.paket.unity.internal.DefaultPaketUnityPluginExtension
+
+class PaketUnitySetupExtension extends AbstractAnnotationDrivenExtension<PaketUnity> {
+    @Override
+    void visitFieldAnnotation(PaketUnity annotation, FieldInfo field) {
+        def interceptor
+        interceptor = new PaketUnitySetupInterceptor(field.name, annotation.projectName(), annotation.projectReferences())
+        interceptor.install(field.parent.getTopSpec())
+        interceptor.install(field.parent.getBottomSpec())
+    }
+}
+
+@InheritConstructors
+class PaketUnitySetupInterceptor extends GradleIntegrationSpecInterceptor implements PaketUnitySetup {
+
+    protected final String projectName
+
+    File projectReferencesFile
+
+    List<String> projectReferences
+
+    @Override
+    String getName() {
+        return projectName
+    }
+
+    PaketUnitySetupInterceptor(String fieldName, String projectName, String[] projectReferences) {
+        super(fieldName)
+        this.projectName = projectName ?: fieldName
+        this.projectReferences = projectReferences.toList()
+    }
+
+    void setupProject(IMethodInvocation invocation) {
+        createOrUpdateReferenceFile()
+    }
+
+    @Override
+    File getInstallDirectory() {
+        new File(unityProjectDir, "/Assets/${DefaultPaketUnityPluginExtension.DEFAULT_PAKET_DIRECTORY}")
+    }
+
+    @Override
+    File getUnityProjectDir() {
+        new File(projectDir, projectName)
+    }
+
+    File createOrUpdateReferenceFile() {
+        def path = "${projectName}/${DefaultPaketUnityPluginExtension.DEFAULT_PAKET_UNITY_REFERENCES_FILE_NAME}"
+        def referencesFile = createFile(path)
+        referencesFile.text = """${projectReferences.join("\r")}""".stripIndent()
+        this.projectReferencesFile = referencesFile
+        referencesFile
+    }
+
+    File createOrUpdateReferenceFile(List<String> projectReferences) {
+        this.projectReferences = projectReferences
+        createOrUpdateReferenceFile()
+    }
+}
